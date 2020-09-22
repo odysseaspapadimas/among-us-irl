242ede7434b0f9ad42bbfd2e52703208cdca53a0
unknown <odysseas.patra@gmail.com>
2020-09-22 19:53:34 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,115 @@
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
+var path = require("path");
+
+const app = express();
+
+app.set("port", 5000);
+app.use(express.static(`${__dirname}/static`));
+
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get("/", function (request, response) {
+  response.sendFile(path.join(__dirname, "/index.html"));
+});
+
+server.listen(5500, () => {
+  console.log("Server ready...");
+});
+
+let players = [];
+let ready = [];
+let roles = ["impostor", 'miet'];
+
+io.on("connection", (socket) => {
+  socket.on("print", (msg) => {
+    console.log(msg);
+  });
+
+  socket.on("ready", (player) => {
+    ready.push(player);
+    console.log(ready);
+    io.emit("ready", ready);
+
+    if (arraysEqual(players, ready)) {
+      for (let i = 0; i < players.length; i++) {
+        shuffle(roles);
+        let role = roles.splice(Math.floor(Math.random() * roles.length), 1);
+        io.to(players[i]).emit("role", role);
+      }
+    }
+  });
+
+  socket.on("emergency", (msg) => {
+    io.emit("emergency", msg);
+  });
+
+  let colors = ['black', 'blue', 'brown', 'green', 'orange', 'pink', 'purple', 'red', 'white', 'yellow'];
+  
+
+  socket.on("nameSubmit", (nickname) => {
+    console.log(`${nickname} joined the game`);
+    socket.emit("nameSubmit", `Welcome ${nickname}!`);
+    socket.broadcast.emit("joined", `${nickname} joined the game`);
+    socket.nickname = nickname;
+
+    players.push(nickname);
+    socket.join(nickname);
+    console.log(players);
+
+    let color = colors.splice(Math.floor(Math.random() * colors.length), 1);
+    console.log(color);
+    io.to(socket.nickname).emit('color', color);
+    io.emit("players", players);
+  });
+
+  socket.on("disconnect", () => {
+    removePlayer(socket.nickname);
+    io.emit("disconnected", socket.nickname, players, ready);
+  });
+});
+
+function removePlayer(player) {
+  players = players.filter((e) => e !== player);
+  ready = ready.filter((e) => e !== player);
+}
+
+function arraysEqual(_arr1, _arr2) {
+  if (
+    !Array.isArray(_arr1) ||
+    !Array.isArray(_arr2) ||
+    _arr1.length !== _arr2.length
+  )
+    return false;
+
+  var arr1 = _arr1.concat().sort();
+  var arr2 = _arr2.concat().sort();
+
+  for (var i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+}
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
---
+++ static/client.js
@@ -0,0 +1,95 @@
+const emergencyBtn = document.querySelector(".emergency");
+const name = document.querySelector(".name");
+const nameSubmit = document.querySelector(".name-submit");
+const nameModal = document.querySelector(".name-modal");
+const emergencyMsg = document.querySelector(".emergency-msg");
+const readyBtn = document.querySelector(".ready");
+const readyUl = document.querySelector(".players-ready");
+const playersUl = document.querySelector(".players");
+const roleH = document.querySelector(".role");
+
+const emergency = new Audio("/sounds/emergency.wav");
+const body = new Audio("/sounds/body.wav");
+const roles = new Audio("/sounds/roles.wav");
+
+let nickname;
+let playerColor;
+
+
+const socket = io();
+
+name.focus();
+
+readyBtn.addEventListener("click", () => {
+  socket.emit("ready", nickname);
+
+  readyBtn.disabled = true;
+  readyBtn.style.backgroundColor = `green`;
+});
+
+socket.on("ready", (readyPlayers) => {
+  readyUl.innerHTML = ``;
+  for (let i = 0; i < readyPlayers.length; i++) {
+    readyUl.innerHTML += `<li class="player">${readyPlayers[i]} is ready!</li>`;
+  }
+});
+
+emergencyBtn.addEventListener("click", () => {
+  socket.emit("emergency", nickname);
+});
+
+socket.on("emergency", (msg) => {
+  emergencyMsg.innerText = `${msg} called an emergency meeting!`;
+  emergency.play();
+});
+
+nameSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  nickname = name.value;
+  socket.emit("nameSubmit", nickname);
+});
+
+socket.on("nameSubmit", (msg) => {
+  console.log(msg);
+  nameModal.style.display = "none";
+});
+
+socket.on("joined", (msg) => {
+  console.log(msg);
+});
+
+socket.on('color', color => {
+    playerColor = color;
+    console.log(playerColor);
+})
+socket.on("players", (players) => {
+  playersUl.innerHTML = ``;
+  for (let i = 0; i < players.length; i++) {
+    playersUl.innerHTML += `<div class="player">
+                                <li>${players[i]}</li>
+                                <img src="img/characters/${playerColor}.png" alt="">
+                            </div>`;
+  }
+});
+
+socket.on("disconnect", (data) => {
+  socket.emit("print", data);
+});
+
+socket.on("disconnected", (nickname, players, readyPlayers) => {
+  console.log(`${nickname} disconnected`);
+  playersUl.innerHTML = ``;
+  for (let i = 0; i < players.length; i++) {
+    playersUl.innerHTML += `<li class="player">${players[i]}</li>`;
+  }
+
+  readyUl.innerHTML = ``;
+  for (let i = 0; i < players.length; i++) {
+    readyUl.innerHTML += `<li class="player">${readyPlayers[i]} is ready!</li>`;
+  }
+});
+
+socket.on("role", (role) => {
+  roleH.innerHTML = role;
+  roles.play();
+});
